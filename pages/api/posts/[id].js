19c7f8f0b14b2d5002dae0ec8c7585a52b0820f0
My Lhassane <mastinoz_7@hotmail.com>
2022-08-11 16:48:00 +0100

    Changed JSON URL to Github file

--- pages/api/posts/[id].js
+++ pages/api/posts/[id].js
@@ -1,4 +1,4 @@
-import {posts} from '../../../posts';
+import {posts} from 'https://raw.githubusercontent.com/MyLhassane/next-02-myapp/5c9ae860935ce76c3d44e458bad6e68dc733c98f/posts.js';
 
 export default function handler(req, res) {
 
