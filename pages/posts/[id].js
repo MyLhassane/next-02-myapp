05ed329fc779dad46a72c343a5fee140970d02a2
My Lhassane <mastinoz_7@hotmail.com>
2022-08-11 15:29:17 +0100

    getStaticProps & getStaticPaths

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -11,9 +11,28 @@ export default function Details(props) {
   )
 }
 
-export async function getServerSideProps(context) {
+export async function getStaticPaths() {
+  const res = await fetch("http://jsonplaceholder.typicode.com/posts/?_limit=10");
+  const data = await res.json();
+
+  const paths = data.map(d => {
+    return {
+      params: {id: `${d.id}`}
+    }
+  })
+
+  return {
+    paths: paths,
+    fallback: false,
+  }
+
+}
+
+export async function getStaticProps(context) {
   const res = await fetch(`http://jsonplaceholder.typicode.com/posts/${context.params.id}`)
   const data = await res.json();
+
+  
 
   return {
     props: {
